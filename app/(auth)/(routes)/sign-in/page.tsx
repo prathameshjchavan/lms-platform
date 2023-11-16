a13518d5ac660a84b3d2fe13f2d7de232dc0a091
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-16 17:16:34 +0530

    setup authentication

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const SignInPage = (props: Props) => {
-	return <div>This is an unprotected page</div>;
-};
-
-export default SignInPage;
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+type Props = { children: React.ReactNode };
+
+const layout = ({ children }: Props) => {
+	return (
+		<div className="h-full flex items-center justify-center">{children}</div>
+	);
+};
+
+export default layout;
