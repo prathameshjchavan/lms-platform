14b586e1f6605f2feaf4d64d296eebf529efb990
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-16 18:00:37 +0530

    added navbar and mobile sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./_components/Navbar";
 import Sidebar from "./_components/Sidebar";
 
 type Props = { children: React.ReactNode };
@@ -5,6 +6,9 @@ type Props = { children: React.ReactNode };
 const layout = ({ children }: Props) => {
 	return (
 		<div className="h-full">
+			<div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+				<Navbar />
+			</div>
 			<div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
 				<Sidebar />
 			</div>
