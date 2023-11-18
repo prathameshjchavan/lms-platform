39c90d5b80e43c4e668c25a8c70fa3e709286732
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-18 17:12:44 +0530

    added category combobox

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
+import CategoryForm from "./_components/category-form";
 
 interface Props {
 	params: { courseId: string };
@@ -18,6 +19,12 @@ const CoursePage = async ({ params: { courseId } }: Props) => {
 
 	const course = await db.course.findUnique({
 		where: { id: courseId, userId },
+	});
+
+	const categories = await db.category.findMany({
+		orderBy: {
+			name: "asc",
+		},
 	});
 
 	if (!course) return redirect("/");
@@ -57,6 +64,14 @@ const CoursePage = async ({ params: { courseId } }: Props) => {
 						initialData={course}
 						courseId={course.id}
 						userId={userId}
+					/>
+					<CategoryForm
+						initialData={course}
+						courseId={course.id}
+						options={categories.map((category) => ({
+							label: category.name,
+							value: category.id,
+						}))}
 					/>
 				</div>
 			</div>
