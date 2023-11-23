378c021743622e4ac9e19ff5759ceeb8075200fb
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-23 13:49:20 +0530

    implemented delete chapter api route

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { UTApi } from "uploadthing/server";
+
+const utapi = new UTApi({
+	fetch: globalThis.fetch,
+	apiKey: process.env.UPLOADTHING_SECRET,
+});
 
 export async function PATCH(
 	req: Request,
@@ -32,6 +38,68 @@ export async function PATCH(
 		return NextResponse.json(chapter);
 	} catch (error) {
 		console.log("[COURSES_CHAPTER_ID]", error);
+		return new NextResponse("Internal Error", { status: 500 });
+	}
+}
+
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { courseId: string; chapterId: string } }
+) {
+	try {
+		const { userId } = auth();
+
+		if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+		const ownCourse = await db.course.findUnique({
+			where: {
+				id: params.courseId,
+				userId,
+			},
+		});
+
+		if (!ownCourse) return new NextResponse("Unauthorized", { status: 401 });
+
+		const chapter = await db.chapter.findUnique({
+			where: {
+				id: params.chapterId,
+				courseId: params.courseId,
+			},
+		});
+
+		if (!chapter) return new NextResponse("Not found", { status: 404 });
+
+		if (chapter.videoUrl && chapter.videoKey) {
+			await utapi.deleteFiles(chapter.videoKey);
+		}
+
+		const deletedChapter = await db.chapter.delete({
+			where: {
+				id: params.chapterId,
+			},
+		});
+
+		const publishedChaptersInCourse = await db.chapter.findMany({
+			where: {
+				courseId: params.courseId,
+				isPublished: true,
+			},
+		});
+
+		if (!publishedChaptersInCourse.length) {
+			await db.course.update({
+				where: {
+					id: params.courseId,
+				},
+				data: {
+					isPublished: false,
+				},
+			});
+		}
+
+		return NextResponse.json(deletedChapter);
+	} catch (error) {
+		console.log("[CHAPTER_ID_DELETE]", error);
 		return new NextResponse("Internal Error", { status: 500 });
 	}
 }
