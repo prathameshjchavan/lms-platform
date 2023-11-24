45fd4e752fe323401ba509c4b810d3961a44f408
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-24 17:56:54 +0530

    added row actions in data table

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -3,7 +3,16 @@
 import { Button } from "@/components/ui/button";
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 export const columns: ColumnDef<Course>[] = [
 	{
@@ -45,6 +54,31 @@ export const columns: ColumnDef<Course>[] = [
 					Published
 					<ArrowUpDown className="ml-2 h-4 w-4" />
 				</Button>
+			);
+		},
+	},
+	{
+		id: "actions",
+		cell: ({ row }) => {
+			const { id } = row.original;
+
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<Link href={`/teacher/courses/${id}`}>
+							<DropdownMenuItem>
+								<Pencil className="h-4 w-4 mr-2" />
+								Edit
+							</DropdownMenuItem>
+						</Link>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			);
 		},
 	},
