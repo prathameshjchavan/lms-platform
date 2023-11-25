585e56869835c0382747b503d5cc20f8338e710e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-25 16:43:18 +0530

    created chapter page

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,12 @@
+interface ChapterIdPageProps {
+	params: {
+		courseId: string;
+		chapterId: string;
+	};
+}
+
+const ChapterIdPage = ({ params }: ChapterIdPageProps) => {
+	return <div>ChapterIdPage</div>;
+};
+
+export default ChapterIdPage;
