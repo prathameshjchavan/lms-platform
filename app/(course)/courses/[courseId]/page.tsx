3b5615f70712b869a738ad09d6066bce899ef19b
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-25 12:55:12 +0530

    added course id page with layout

---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface CourseIdPageProps {
+	params: {
+		courseId: string;
+	};
+}
+
+const CourseIdPage = ({ params }: CourseIdPageProps) => {
+	return <div>CourseIdPage</div>;
+};
+
+export default CourseIdPage;
