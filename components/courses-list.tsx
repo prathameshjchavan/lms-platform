d6e9493aa54303e8ad40f935336e3b8dbf966c5b
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-25 12:41:33 +0530

    implemented course card

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -1,4 +1,5 @@
 import { Category, Course } from "@prisma/client";
+import CourseCard from "./course-card";
 
 type CourseWithProgressAndCategory = Course & {
 	category: Category | null;
@@ -13,9 +14,25 @@ interface CourseListProps {
 const CoursesList = ({ items }: CourseListProps) => {
 	return (
 		<div>
-			{items.map((item) => (
-				<div key={item.id}>{item.title}</div>
-			))}
+			<div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+				{items.map((item) => (
+					<CourseCard
+						key={item.id}
+						id={item.id}
+						title={item.title}
+						imageUrl={item.imageUrl!}
+						chaptersLength={item.chapters.length}
+						price={item.price!}
+						progress={item.progress}
+						category={item.category?.name!}
+					/>
+				))}
+			</div>
+			{!items.length && (
+				<div className="text-center text-sm text-muted-foreground mt-10">
+					No courses found
+				</div>
+			)}
 		</div>
 	);
 };
