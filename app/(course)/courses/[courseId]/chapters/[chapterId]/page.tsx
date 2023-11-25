1e48cdd1c16f455a208ea1dc881797d651912b4b
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-25 17:06:54 +0530

    added banner to chapter page

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,3 +1,8 @@
+import { getChapter } from "@/actions/get-chapter";
+import Banner from "@/components/Banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 interface ChapterIdPageProps {
 	params: {
 		courseId: string;
@@ -5,8 +10,36 @@ interface ChapterIdPageProps {
 	};
 }
 
-const ChapterIdPage = ({ params }: ChapterIdPageProps) => {
-	return <div>ChapterIdPage</div>;
+const ChapterIdPage = async ({ params }: ChapterIdPageProps) => {
+	const { userId } = auth();
+
+	if (!userId) return redirect("/");
+
+	const { chapter, course, attachments, nextChapter, userProgress, purchase } =
+		await getChapter({
+			userId,
+			chapterId: params.chapterId,
+			courseId: params.courseId,
+		});
+
+	if (!chapter || !course) return redirect("/");
+
+	const isLocked = !chapter.isFree && !purchase;
+	const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+	return (
+		<div>
+			{userProgress?.isCompleted && (
+				<Banner variant="success" label="You already completed this chapter." />
+			)}
+			{isLocked && (
+				<Banner
+					variant="warning"
+					label="You need to purchase this course to watch this chapter."
+				/>
+			)}
+		</div>
+	);
 };
 
 export default ChapterIdPage;
