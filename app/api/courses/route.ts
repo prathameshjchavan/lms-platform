e1814b76df1446c5d273be201b9ef0fb84b76fd3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-27 21:42:18 +0530

    added admin protection for api routes

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,13 +1,16 @@
 import { db } from "@/lib/db";
+import { isTeacher } from "@/lib/teacher";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
 	try {
 		const { userId } = auth();
-		const { title } = await req.json();
 
-		if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+		if (!userId || !isTeacher(userId))
+			return new NextResponse("Unauthorized", { status: 401 });
+
+		const { title } = await req.json();
 
 		const course = await db.course.create({
 			data: { userId, title },
